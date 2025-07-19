2acd599ff3ab166e4d1e80968f9b8f5ba5d7940a
AdityaBare <bareaditya0@gmail.com>
2025-07-19 11:08:19 +0530

    Sigup route completed

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+
+require('dotenv').config();
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const userRoute = require('./router/user')
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
-
+app.use('/',userRoute);
 
 
 app.listen(8080,()=>{
-    // mongoose.connect(process.env.Mongooose_URL);
+    mongoose.connect(process.env.Mongooose_URL);
     console.log("listening on port 8080");
 })
